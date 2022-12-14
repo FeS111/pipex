e966ca296dad5b7e8beeb6baad54d28a96af42af
fschmid <fschmid@student.42heilbronn.de>
2022-12-14 11:17:14 +0100

    mando

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 14:03:49 by fschmid           #+#    #+#             */
-/*   Updated: 2022/12/13 16:56:29 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/12/14 11:16:58 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(int argc, char **argv, char **env)
 	if (fd < 0)
 		ft_exit("", 0);
 	pipex(argc, argv, env, fd);
-	// system("leaks pipex");
+	system("leaks pipex");
 	return (0);
 }
 
@@ -39,13 +39,13 @@ void	pipex(int argc, char **argv, char **env, int fd)
 		fd = spawn_child(fd, argv[i++], env);
 		if (fd < 0)
 			ft_exit("Spawing of child proccess failed", 0);
-		i++;
 	}
 	waitpid(0, NULL, 0);
 	output = read_fd(fd);
 	write_output(argc, argv, output);	
 	close(fd);
 	close(out_fd);
+	free(output);
 }
 
 void	write_output(int argc, char **argv, char *output)
